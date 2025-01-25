d2fd310d261f45e37b35f90f5c228c0d65c93b43
clickthisstudios <mofosatacok@gmail.com>
2025-01-25 12:41:09 -0600

    player movement

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from player import Player
@@ -8,17 +5,31 @@ from player import Player
 def main():
      pygame.init()
      screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-     clock = pygame.time.Clock()
-     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2) 
+     clock = pygame.time.Clock() 
+     updatable = pygame.sprite.Group()
+     drawable = pygame.sprite.Group()
+     Player.containers = (updatable, drawable)
+     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
      dt = 0
-     
+          
      while True:
           for event in pygame.event.get():
                if event.type == pygame.QUIT:
                     return
+               
+          for obj in updatable:
+               obj.update(dt)
+
+          player.update(dt)
+
           screen.fill("black")
+          for obj in drawable:
+               obj.draw(screen)
+
           player.draw(screen)
+
           pygame.display.flip()
+
           dt = clock.tick(60)
      
 
